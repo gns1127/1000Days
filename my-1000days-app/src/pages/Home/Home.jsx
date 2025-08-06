f92fa03243f81d0684311a4fe6a1965fdf54c489
ParkSangHun <tkdgns1127@naver.com>
2025-08-06 10:03:57 +0900

    무한스크롤 개발 2

--- my-1000days-app/src/pages/Home/Home.jsx
+++ my-1000days-app/src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useCallback} from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FaClock, FaMapMarkerAlt, FaHome, FaPlus, FaUser } from 'react-icons/fa';
 import { useAuth } from '../../features/auth/useAuth';
@@ -22,21 +22,22 @@ const Home = () => {
   const [loading, setLoading] = useState(true);
   const [selectedFeedId, setSelectedFeedId] = useState(null); // 선택된 피드 id
   const observerRef = useRef();
+  const feedIdSetRef = useRef(new Set()); // ✅ 중복 방지용
 
   const fetchFeeds = useCallback(async () => {
-    if (!user && !hasMore ) return; // 로그인된 사용자 없으면 대기
+    if (!user && !hasMore) return; // 로그인된 사용자 없으면 대기
 
     const from = page * LIMIT;
     const to = from + LIMIT - 1;
 
-    console.log( 'form :: '  + from );
-    console.log( 'to :: ' + to  );
+    console.log('form :: ' + from);
+    console.log('to :: ' + to);
     // 방어 코드: from 또는 to가 음수가 되면 종료
     if (from < 0 || to < 0) return;
 
     const { data, error } = await supabase
       .from('feeds')
-      .select(`id, title, feed_date, feed_photos(photo_url, display_order)`)
+      .select(`id, title, feed_date, feed_photos(photo_url, display_order)`, { count: 'exact' })
       .eq('user_id', user.id)
       .order('feed_date', { ascending: false })
       .range(from, to);
@@ -45,8 +46,27 @@ const Home = () => {
       console.error('피드 조회 오류:', error);
       return;
     } else {
+
+      const newFeeds = [];
+
+      for (const feed of data) {
+        if (feedIdSetRef.current.has(feed.id)) continue;
+        feedIdSetRef.current.add(feed.id);
+
+        const photos = feed.feed_photos || [];
+        photos.sort((a, b) => a.display_order - b.display_order);
+
+        newFeeds.push({
+          id: feed.id,
+          title: feed.title,
+          date: feed.feed_date.slice(0, 10),
+          repPhoto: photos[0]?.photo_url || ''
+        })
+
+      }
       //console.log( data );
-      const processed = data.map(feed => {
+
+      /*const processed = data.map(feed => {
         const photos = feed.feed_photos || [];
         photos.sort((a, b) => a.display_order - b.display_order);
         return {
@@ -56,35 +76,35 @@ const Home = () => {
           repPhoto: photos[0]?.photo_url || ''
         };
       });
+      */
 
-
-      setFeeds(prev => [...prev, ...processed]);
+      setFeeds(prev => [...prev, ...newFeeds]);
       if (data.length < LIMIT) setHasMore(false);
       setLoading(false);
     }
   }, [page, user, hasMore]);
 
   useEffect(() => {
-    if( user && hasMore ) fetchFeeds();
+    if (user && hasMore) fetchFeeds();
   }, [page, user]);
 
   useEffect(() => {
     const observer = new IntersectionObserver(
       entries => {
-        if( entries[0].isIntersecting && hasMore){
+        if (entries[0].isIntersecting && hasMore) {
           setPage(prev => prev + 1);
         }
       },
-      { threshold: 1}
+      { threshold: 1 }
     );
 
-    if( observerRef.current){
-      observer.observe( observerRef.current);
+    if (observerRef.current) {
+      observer.observe(observerRef.current);
     }
 
     return () => {
-      if( observerRef.current){
-        observer.unobserve( observerRef.current);
+      if (observerRef.current) {
+        observer.unobserve(observerRef.current);
       }
     }
   }, [hasMore]);
